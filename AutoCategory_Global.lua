106e9b3457530c6dfb0b3cdde41f1608e9ce79cb
Dolores Scott <shadowfen@gmail.com>
2020-04-06 19:19:55 -0500

    Add Russian language strings provided by Ckau

--- AutoCategory_Global.lua
+++ AutoCategory_Global.lua
@@ -9,7 +9,7 @@ local SF = LibSFUtils
  
 AutoCategory = {
     name = "AutoCategory",
-    version = "2.4.4",
+    version = "2.4.5",
     settingName = "AutoCategory",
     settingDisplayName = "AutoCategory - Revised",
     author = "Shadowfen, crafty35, RockingDice",
