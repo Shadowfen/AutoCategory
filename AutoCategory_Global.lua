cc0be353acdaaef186ba0a8f8ed556107639cc6f
Dolores Scott <scottd@texas.net>
2021-12-19 22:19:54 -0600

    add "armor_invigorating", v 2.34

--- AutoCategory_Global.lua
+++ AutoCategory_Global.lua
@@ -9,7 +9,7 @@ local SF = LibSFUtils
  
 AutoCategory = {
     name = "AutoCategory",
-    version = "2.33",
+    version = "2.34",
     settingName = "AutoCategory",
     settingDisplayName = "AutoCategory - Revised",
     author = "Shadowfen, crafty35, RockingDice, Friday_the13_rus",
