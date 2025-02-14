b91459a64b26f6990c0f6104507dde74c9024eb7
Dolores Scott <shadowfen@protonmail.com>
2025-02-13 19:23:06 -0600

    ver 4.3.2

--- AutoCategory_Global.lua
+++ AutoCategory_Global.lua
@@ -9,7 +9,7 @@ local SF = LibSFUtils
  
 AutoCategory = {
     name = "AutoCategory",
-    version = "4.3.1",
+    version = "4.3.2",
     settingName = "AutoCategory",
     settingDisplayName = "AutoCategory - Revised",
     author = "Shadowfen, crafty35, RockingDice, Friday_the13_rus",
